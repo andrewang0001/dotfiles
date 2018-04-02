d9f26f7be2d0d4357ff6940901675e3b6505c75c
Andre Wang <andre@andre-desktop.lan>
2018-04-02 15:10:12 +0800

    added logic

--- update.sh
+++ update.sh
@@ -9,14 +9,13 @@ dnf upgrade -y
 FILENAME="rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
 RPMFUSION="https://download1.rpmfusion.org/free/fedora/$(FILENAME)"
 
-wget $(RPMFUSION)
-dnf install -y $(FILENAME)
+wget $(RPMFUSION) && dnf install -y $(FILENAME)
 
 ## MS fonts
 MSTTFILE="msttcore-fonts-installer-2.6-1.noarch.rpm"
 MSTT="https://downloads.sourceforge.net/project/mscorefonts2/rpms/$(MSTTFILE)"
 
-dnf install -y $(MSTT)
+wget $(MSTT) && dnf install -y $(MSTT)
 
 ## Stuff to install ...
 dnf install -y keepassxc fuse-exfat clementine sqlite wget cabextract aspell emacs vim-enhanced texlive-scheme-full
