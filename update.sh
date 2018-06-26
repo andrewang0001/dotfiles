521b6f46f94461a10293b33bcc6077d516019f90
Andre <andre.wang0001@gmail.com>
2018-06-26 15:45:22 +0800

    updated software list

--- update.sh
+++ update.sh
@@ -11,6 +11,10 @@ RPMFUSION="https://download1.rpmfusion.org/free/fedora/$(FILENAME)"
 
 wget $(RPMFUSION) && dnf install -y $(FILENAME)
 
+install_list=(keepassxc fuse-exfat clementine sqlite wget cabextract aspell emacs vim-enhanced texlive-scheme-full mozilla-fira* R-base gcc-c++ gcc-fortran)
+install_list_rpmfusion=(gstreamer-plugin-bad gstreamer-plugin-ugly)
+wm=(i3 i3status i3lock sway)
+
 ## MS fonts
 MSTTFILE="msttcore-fonts-installer-2.6-1.noarch.rpm"
 MSTT="https://downloads.sourceforge.net/project/mscorefonts2/rpms/$(MSTTFILE)"
@@ -18,6 +22,6 @@ MSTT="https://downloads.sourceforge.net/project/mscorefonts2/rpms/$(MSTTFILE)"
 wget $(MSTT) && dnf install -y $(MSTT)
 
 ## Stuff to install ...
-dnf install -y keepassxc fuse-exfat clementine sqlite wget cabextract aspell emacs vim-enhanced texlive-scheme-full
-dnf install -y gstreamer-plugin-bad gstreamer-plugin-ugly R-base gcc-c++ gcc-fortran
-dnf install -y "mozilla-fira*"
+dnf install -y ${install_list[@]}
+dnf install -y ${install_list_rpmfusion[@]}
+dnf install -y ${wm[@]}
