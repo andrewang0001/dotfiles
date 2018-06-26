320d408990cbe2cd3bd418c7554f556306380c5d
Andre <andre.wang0001@gmail.com>
2018-06-26 15:39:59 +0800

    beautified script

--- linkFiles.sh
+++ linkFiles.sh
@@ -1,13 +1,56 @@
 #!/bin/bash
 
 ## Link all files to home.
+CMD="$1"
 
 this_dir=$(pwd)
 GLOBIGNORE=".:.."
+dotfiles=(.emacs .vimrc .Xdefaults)
+dotfiles_config=(i3 i3status)
 
-for file in .*; do
-  if [[ "$file" != ".git" ]]; then
-    echo "Linking $file."
-    ln -s "$this_dir/$file" "$HOME"
-  fi
-done
+install() {
+
+  ## Begin with the files that belong in ~/
+  for f in ${dotfiles[@]}; do
+    if [[ -f $HOME/$f ]]; then
+      echo "$HOME/$f exists! Deleting."
+      rm -rf "$HOME/$f"
+    fi
+    echo "ln -s $this_dir/$f $HOME/$f"
+    ln -s "$this_dir/$f" "$HOME/$f"
+  done
+
+  ## Now also do the files that belong in ~/.config
+  mkdir -p "$HOME/.config"
+  for f in ${dotfiles_config[@]}; do
+    if [[ -f $HOME/.config/$f ]]; then
+      echo "$HOME/.config/$f exists! Deleting."
+      echo "rm -rf "$HOME/.config/$f""
+    fi
+    echo "ln -s $this_dir/.config/$f $HOME/.config/$f"
+    ln -s $this_dir/.config/$f $HOME/.config/$f
+  done
+}
+
+print_help() {
+  prog=$(basename "$0")
+  echo "Usage: $prog [-option]"
+  echo "--help: print this message"
+  echo "-i:     install dotfiles"
+}
+
+case "$CMD" in
+  --help)
+    print_help
+    exit 0
+    ;;
+      -i)
+    install
+    exit 0
+    ;;
+    *)
+    print_help
+    exit 1
+    ;;
+esac
+install
